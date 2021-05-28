a2a479829afae244471b4da4e000350a193023a4
Lachlan <lachlanmcclymont25@gmail.com>
2021-05-28 13:11:01 +1000

    Update test_predefined.py to output each test
    Altered test_predefined() so that it runs the tests. Displays each test as they're being carried out. Also fixed a bug for divide by zero.

--- test_predefined.py
+++ test_predefined.py
@@ -1,6 +1,6 @@
 import unittest
 import api
-
+import math
 
 class TestCase:
     """ TestCase class holds information about individual test cases """
@@ -25,6 +25,8 @@ class TestCase:
         return result, result + 0.0
 
     def calc_divide(self):
+        if self.lhs == 0 or self.rhs == 0:
+            return 0, float('nan')
         result = self.lhs / self.rhs
         return result, result + 0.0
 
@@ -37,9 +39,9 @@ class TestData:
     """ TestData class generates the test data, and compares it """
 
     """ List of test values of each integer type"""
-    positive_even_integers = []
+    positive_even_integers = [0]
     positive_odd_integers = []
-    negative_even_integers = []
+    negative_even_integers = [0]
     negative_odd_integers = []
 
     """ Note:
@@ -47,8 +49,8 @@ class TestData:
         thus single lists are created for both positive & negative decimals """
 
     """ List of test values for each decimal type """
-    positive_decimals = []
-    negative_decimals = []
+    positive_decimals = [0]
+    negative_decimals = [0]
 
     """ Test cases, mixed contains test cases which include both integers and decimals """
     test_cases_integers = []
@@ -119,29 +121,17 @@ class TestData:
         """ Generate test cases for: LHS = Positive Decimal, RHS = Positive Decimal """
         for i in range(1, _range):
             self.test_cases_decimals.append(TestCase(self.positive_decimals[i], self.positive_decimals[i]))
-            self.test_cases_decimals.append(TestCase(self.positive_decimals[i], self.positive_decimals[i]))
-            self.test_cases_decimals.append(TestCase(self.positive_decimals[i], self.positive_decimals[i]))
-            self.test_cases_decimals.append(TestCase(self.positive_decimals[i], self.positive_decimals[i]))
 
         """ Generate test cases for: LHS = Negative Decimal, RHS = Negative Decimal """
         for i in range(1, _range):
-            self.test_cases_decimals.append(TestCase(self.negative_decimals[i], self.negative_decimals[i]))
-            self.test_cases_decimals.append(TestCase(self.negative_decimals[i], self.negative_decimals[i]))
-            self.test_cases_decimals.append(TestCase(self.negative_decimals[i], self.negative_decimals[i]))
             self.test_cases_decimals.append(TestCase(self.negative_decimals[i], self.negative_decimals[i]))
 
         """ Generate test cases for: LHS = Positive Decimal, RHS = Negative Decimal """
         for i in range(1, _range):
             self.test_cases_decimals.append(TestCase(self.positive_decimals[i], self.negative_decimals[i]))
-            self.test_cases_decimals.append(TestCase(self.positive_decimals[i], self.negative_decimals[i]))
-            self.test_cases_decimals.append(TestCase(self.positive_decimals[i], self.negative_decimals[i]))
-            self.test_cases_decimals.append(TestCase(self.positive_decimals[i], self.negative_decimals[i]))
 
         """ Generate test cases for: LHS = Negative Decimal, RHS = Positive Decimal """
         for i in range(1, _range):
-            self.test_cases_decimals.append(TestCase(self.negative_decimals[i], self.positive_decimals[i]))
-            self.test_cases_decimals.append(TestCase(self.negative_decimals[i], self.positive_decimals[i]))
-            self.test_cases_decimals.append(TestCase(self.negative_decimals[i], self.positive_decimals[i]))
             self.test_cases_decimals.append(TestCase(self.negative_decimals[i], self.positive_decimals[i]))
 
         """ MIXED """
@@ -149,21 +139,15 @@ class TestData:
         """ Generate test cases for: LHS = Positive Integer, RHS = Positive Decimal """
         for i in range(1, _range):
             self.test_cases_mixed.append(TestCase(self.positive_even_integers[i], self.positive_decimals[i]))
-            self.test_cases_mixed.append(TestCase(self.positive_even_integers[i], self.positive_decimals[i]))
-            self.test_cases_mixed.append(TestCase(self.positive_odd_integers[i], self.positive_decimals[i]))
             self.test_cases_mixed.append(TestCase(self.positive_odd_integers[i], self.positive_decimals[i]))
 
         """ Generate test cases for: LHS = Negative Integer, RHS = Negative Decimal """
         for i in range(1, _range):
             self.test_cases_mixed.append(TestCase(self.negative_even_integers[i], self.negative_decimals[i]))
-            self.test_cases_mixed.append(TestCase(self.negative_even_integers[i], self.negative_decimals[i]))
-            self.test_cases_mixed.append(TestCase(self.negative_odd_integers[i], self.negative_decimals[i]))
             self.test_cases_mixed.append(TestCase(self.negative_odd_integers[i], self.negative_decimals[i]))
 
         """ Generate test cases for: LHS = Positive Decimal, RHS = Positive Integer """
         for i in range(1, _range):
-            self.test_cases_mixed.append(TestCase(self.positive_decimals[i], self.positive_even_integers[i]))
-            self.test_cases_mixed.append(TestCase(self.positive_decimals[i], self.positive_odd_integers[i]))
             self.test_cases_mixed.append(TestCase(self.positive_decimals[i], self.positive_even_integers[i]))
             self.test_cases_mixed.append(TestCase(self.positive_decimals[i], self.positive_odd_integers[i]))
 
@@ -171,41 +155,33 @@ class TestData:
         for i in range(1, _range):
             self.test_cases_mixed.append(TestCase(self.negative_decimals[i], self.negative_even_integers[i]))
             self.test_cases_mixed.append(TestCase(self.negative_decimals[i], self.negative_odd_integers[i]))
-            self.test_cases_mixed.append(TestCase(self.negative_decimals[i], self.negative_even_integers[i]))
-            self.test_cases_mixed.append(TestCase(self.negative_decimals[i], self.negative_odd_integers[i]))
 
         """ Generate test cases for: LHS = Positive Integer, RHS = Negative Decimal """
         for i in range(1, _range):
             self.test_cases_mixed.append(TestCase(self.positive_even_integers[i], self.negative_decimals[i]))
-            self.test_cases_mixed.append(TestCase(self.positive_even_integers[i], self.negative_decimals[i]))
-            self.test_cases_mixed.append(TestCase(self.positive_odd_integers[i], self.negative_decimals[i]))
             self.test_cases_mixed.append(TestCase(self.positive_odd_integers[i], self.negative_decimals[i]))
 
         """ Generate test cases for: LHS = Positive Decimal, RHS = Negative Integer """
         for i in range(1, _range):
             self.test_cases_mixed.append(TestCase(self.positive_decimals[i], self.negative_even_integers[i]))
             self.test_cases_mixed.append(TestCase(self.positive_decimals[i], self.negative_odd_integers[i]))
-            self.test_cases_mixed.append(TestCase(self.positive_decimals[i], self.negative_even_integers[i]))
-            self.test_cases_mixed.append(TestCase(self.positive_decimals[i], self.negative_odd_integers[i]))
 
         """ Generate test cases for: LHS = Negative Integer, RHS = Positive Decimal """
         for i in range(1, _range):
             self.test_cases_mixed.append(TestCase(self.negative_even_integers[i], self.positive_decimals[i]))
-            self.test_cases_mixed.append(TestCase(self.negative_even_integers[i], self.positive_decimals[i]))
-            self.test_cases_mixed.append(TestCase(self.negative_odd_integers[i], self.positive_decimals[i]))
             self.test_cases_mixed.append(TestCase(self.negative_odd_integers[i], self.positive_decimals[i]))
 
         """ Generate test cases for: LHS = Negative Decimal, RHS = Positive Integer """
         for i in range(1, _range):
             self.test_cases_mixed.append(TestCase(self.negative_decimals[i], self.positive_even_integers[i]))
             self.test_cases_mixed.append(TestCase(self.negative_decimals[i], self.positive_odd_integers[i]))
-            self.test_cases_mixed.append(TestCase(self.negative_decimals[i], self.positive_even_integers[i]))
-            self.test_cases_mixed.append(TestCase(self.negative_decimals[i], self.positive_odd_integers[i]))
+
+        self.test_cases_decimals = list(dict.fromkeys(self.test_cases_decimals))
 
     def __init__(self,):
         """ Generate test values with _range of values,
             eg, _range = 100, then range of data created is from -50 to 50 """
-        self.range = 1 << 16
+        self.range = 1 << 3
         self.__gen_test_values(self.range)
 
         """ Note:   
@@ -219,12 +195,10 @@ class TestData:
 class TestPredefined(unittest.TestCase):
 
     """ Range of test cases,
-        by default rest cases were created from a numerical range of 1 << 16 (65536)
-        which creates 524,288 test cases for integers, which we'll base the maximum test
-        case range off
-        i,e, range_min = 0, range_max = 10 will only test cases from 0-10 """
+        if range_min = 0, range_max = 10 then this will only test cases from 0-10, providing there are 10 cases """
+
     range_min = 0
-    range_max = 524288
+    range_max = 20
 
     @classmethod
     def setUpClass(cls):
@@ -237,101 +211,156 @@ class TestPredefined(unittest.TestCase):
         for test_case in self.test_data.test_cases_integers[self.range_min:self.range_max]:
             query = str(test_case.lhs) + "+" + str(test_case.rhs)
             result = api.api(query)
-            self.assertTrue(result in test_case.calc_add())
+            passed = result in test_case.calc_add()
+            print('Test: ["' + query + '" == ' + str(test_case.calc_add()[1]) + "] " + str(passed))
+            self.assertTrue(passed)
 
     def test_integer_subtraction(self):
         print("\nPerforming integer subtraction tests..")
         for test_case in self.test_data.test_cases_integers[self.range_min:self.range_max]:
             query = str(test_case.lhs) + "-" + str(test_case.rhs)
             result = api.api(query)
-            self.assertTrue(result in test_case.calc_subtract())
+            passed = result in test_case.calc_subtract()
+            print('Test: ["' + query + '" == ' + str(test_case.calc_subtract()[1]) + "] " + str(passed))
+            self.assertTrue(passed)
 
     def test_integer_multiplication(self):
         print("\nPerforming integer multiplication tests..")
         for test_case in self.test_data.test_cases_integers[self.range_min:self.range_max]:
             query = str(test_case.lhs) + "*" + str(test_case.rhs)
             result = api.api(query)
-            self.assertTrue(result in test_case.calc_multiply())
+            passed = result in test_case.calc_multiply()
+            print('Test: ["' + query + '" == ' + str(test_case.calc_multiply()[1]) + "] " + str(passed))
+            self.assertTrue(passed)
 
     def test_integer_division(self):
         print("\nPerforming integer division tests..")
         for test_case in self.test_data.test_cases_integers[self.range_min:self.range_max]:
             query = str(test_case.lhs) + "/" + str(test_case.rhs)
             result = api.api(query)
-            self.assertTrue(result in test_case.calc_divide())
+            passed = result in test_case.calc_divide()
+            if not passed:
+                if math.isnan(result):
+                    """ Assert false = True """
+                    print('Test: ["' + query + '" == ' + str(test_case.calc_divide()[1]) + "] " + "True")
+                    self.assertFalse(passed)
+            else:
+                print('Test: ["' + query + '" == ' + str(test_case.calc_divide()[1]) + "] " + str(passed))
+                self.assertTrue(passed)
 
     def test_decimal_addition(self):
         print("\nPerforming decimal addition tests..")
         for test_case in self.test_data.test_cases_decimals[self.range_min:self.range_max]:
             query = str(test_case.lhs) + "+" + str(test_case.rhs)
             result = api.api(query)
-            self.assertTrue(result in test_case.calc_add())
+            passed = result in test_case.calc_add()
+            print('Test: ["' + query + '" == ' + str(test_case.calc_add()[1]) + "] " + str(passed))
+            self.assertTrue(passed)
 
     def test_decimal_subtraction(self):
         print("\nPerforming decimal subtraction tests..")
         for test_case in self.test_data.test_cases_decimals[self.range_min:self.range_max]:
             query = str(test_case.lhs) + "-" + str(test_case.rhs)
             result = api.api(query)
-            self.assertTrue(result in test_case.calc_subtract())
+            passed = result in test_case.calc_subtract()
+            print('Test: ["' + query + '" == ' + str(test_case.calc_subtract()[1]) + "] " + str(passed))
+            self.assertTrue(passed)
 
     def test_decimal_multiplication(self):
         print("\nPerforming decimal multiplication tests..")
         for test_case in self.test_data.test_cases_decimals[self.range_min:self.range_max]:
             query = str(test_case.lhs) + "*" + str(test_case.rhs)
             result = api.api(query)
-            self.assertTrue(result in test_case.calc_multiply())
+            passed = result in test_case.calc_multiply()
+            print('Test: ["' + query + '" == ' + str(test_case.calc_multiply()[1]) + "] " + str(passed))
+            self.assertTrue(passed)
 
     def test_decimal_division(self):
         print("\nPerforming decimal division tests..")
         for test_case in self.test_data.test_cases_decimals[self.range_min:self.range_max]:
             query = str(test_case.lhs) + "/" + str(test_case.rhs)
             result = api.api(query)
-            self.assertTrue(result in test_case.calc_divide())
+            passed = result in test_case.calc_divide()
+            if not passed:
+                if math.isnan(result):
+                    """ Assert false = True """
+                    print('Test: ["' + query + '" == ' + str(test_case.calc_divide()[1]) + "] " + "True")
+                    self.assertFalse(passed)
+            else:
+                print('Test: ["' + query + '" == ' + str(test_case.calc_divide()[1]) + "] " + "True")
+                self.assertTrue(passed)
 
     def test_mixed_addition(self):
         print("\nPerforming mixed addition tests..")
         for test_case in self.test_data.test_cases_mixed[self.range_min:self.range_max]:
             query = str(test_case.lhs) + "+" + str(test_case.rhs)
             result = api.api(query)
-            self.assertTrue(result in test_case.calc_add())
+            passed = result in test_case.calc_add()
+            print('Test: ["' + query + '" == ' + str(test_case.calc_add()[1]) + "] " + str(passed))
+            self.assertTrue(passed)
 
     def test_mixed_subtraction(self):
         print("\nPerforming mixed subtraction tests..")
         for test_case in self.test_data.test_cases_mixed[self.range_min:self.range_max]:
             query = str(test_case.lhs) + "-" + str(test_case.rhs)
             result = api.api(query)
-            self.assertTrue(result in test_case.calc_subtract())
+            passed = result in test_case.calc_subtract()
+            print('Test: ["' + query + '" == ' + str(test_case.calc_subtract()[1]) + "] " + str(passed))
+            self.assertTrue(passed)
 
     def test_mixed_multiplication(self):
         print("\nPerforming mixed multiplication tests..")
         for test_case in self.test_data.test_cases_mixed[self.range_min:self.range_max]:
             query = str(test_case.lhs) + "*" + str(test_case.rhs)
             result = api.api(query)
-            self.assertTrue(result in test_case.calc_multiply())
+            passed = result in test_case.calc_multiply()
+            print('Test: ["' + query + '" == ' + str(test_case.calc_multiply()[1]) + "] " + str(passed))
+            self.assertTrue(passed)
 
     def test_mixed_division(self):
         print("\nPerforming mixed division tests..")
         for test_case in self.test_data.test_cases_mixed[self.range_min:self.range_max]:
             query = str(test_case.lhs) + "/" + str(test_case.rhs)
             result = api.api(query)
-            self.assertTrue(result in test_case.calc_divide())
+            passed = result in test_case.calc_divide()
+            if not passed:
+                if math.isnan(result):
+                    """ Assert false = True """
+                    print('Test: ["' + query + '" == ' + str(test_case.calc_divide()[1]) + "] " + "True")
+                    self.assertFalse(passed)
+            else:
+                print('Test: ["' + query + '" == ' + str(test_case.calc_divide()[1]) + "] " + str(passed))
+                self.assertTrue(passed)
 
 
 def test_predefined():
-    
-    print("\nPredefined Tests Executing:")
-    
-    # Using maximum test case range, would take days to run, thus test case range from test case 0 - 10
-    TestPredefined.range_max = 0
-    TestPredefined.range_max = 5
+    """ Executes the tests from another script """
+
+    print("\nPredefined Tests Executing:\n")
+
+    """ Test case range from test case 0 to test case 20 """
+
+    TestPredefined.range_min = 0
+    TestPredefined.range_max = 20
+
+    test_loader = unittest.TestLoader()
+    test_result = unittest.TestResult()
+
+    test_suite = test_loader.loadTestsFromTestCase(TestPredefined)
+    test_suite.run(result=test_result)
+
+    if test_result.wasSuccessful():
+        print("\nPredefined Tests Result: ALL PASSED\n")
+        return True
+    else:
+        print("\nPredefined Tests Result: ALL TESTS DID NOT PASS\n")
+        return False
+
+
+if __name__ == "__main__":
+    """ By using maximum test case range, the test will run for days,  
+        thus test case range from test case 0 to test case 20 """
+    TestPredefined.range_min = 0
+    TestPredefined.range_max = 20
 
     unittest.main()
-    
-    allPassed = True # TODO: return bool true if all predefined tests passed
-    if allPassed:
-        print("Predefined Tests Result: ALL PASSED")
-    else:
-        print("Predefined Tests Result: ALL TESTS DID NOT PASS")
-    
-    allPassed = True
-    return allPassed
