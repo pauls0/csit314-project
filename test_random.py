c15d5a590f26d32d827f4e0bd719acaa0b9c0919
cactuspoint <pollywaffle101@gmail.com>
2021-05-31 13:47:04 +1000

    Remove unneeded files

--- test_random.py
+++ test_random.py
@@ -1,5 +1,20 @@
-from evaluate import evaluate
+from api import api
 import random
+
+# function to check a string with a mathemtical operation matches a known answer
+# input: queryString - mathemtical operation as string
+# input: answerExpected - known answer as float 
+# return: a bool that represents if queryString is equal to answerExpected
+def evaluate(queryString,answerExpected):
+    
+    answerAPI = api(queryString)            # get answer to query from api
+    
+    if (answerAPI == answerExpected):       # set return bool based on if answers match
+        isCorrect = True
+    else:
+        isCorrect = False
+    
+    return isCorrect
 
 def executeTest(queryString,answerExpected):
     
